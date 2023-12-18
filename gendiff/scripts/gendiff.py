20b4849e3ef3c3509f554e1e157c3540506eed45
root <root@DESKTOP-LDK1LH6>
2023-12-18 10:31:36 +0500

    create basic structure

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+
+    # Ваш код для выполнения операций с файлами
+
+if __name__ == '__main__':
+    main()
