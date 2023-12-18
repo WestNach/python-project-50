1bc38333802a3458ef5dbd86b9de9117be754122
root <root@DESKTOP-LDK1LH6>
2023-12-19 00:10:38 +0500

    add gendif script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,8 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', help='First file')
+    parser.add_argument('second_file', help='Second file')
     args = parser.parse_args()
 
     # Ваш код для выполнения операций с файлами
