7997aaee987ef1cdab47f0006626584731b740ca
root <root@DESKTOP-LDK1LH6>
2023-12-21 07:18:00 +0500

    add -f argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 import argparse
 
+def generate_diff(first_file, second_file, format):
+    # Здесь будет ваша логика сравнения файлов
+    pass
 
 def main():
+    # Создаем парсер аргументов командной строки
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', help='First file')
-    parser.add_argument('second_file', help='Second file')
+
+    # Определение позиционных аргументов
+    parser.add_argument('first_file', help='first configuration file')
+    parser.add_argument('second_file', help='second configuration file')
+
+    # Определение опционального аргумента --format
+    parser.add_argument('-f', '--format', help='set format of output')
+
+    # Разбор аргументов командной строки
     args = parser.parse_args()
 
-    # Ваш код для выполнения операций с файлами
-
+    # Вызываем функцию для сравнения файлов
+    generate_diff(args.first_file, args.second_file, args.format)
 
 if __name__ == '__main__':
     main()
