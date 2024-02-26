e732f36a0f9be8ee117699cfae3f7354b349f7ec
root <root@DESKTOP-LDK1LH6>
2024-02-26 17:53:00 +0500

    some fix

--- gendiff/formats/formatting.py
+++ gendiff/formats/formatting.py
@@ -1,15 +1,15 @@
-from gendiff.formats import json
-from gendiff.formats import plain
-from gendiff.formats import stylish
+from gendiff.formats.json import get_json_dict
+from gendiff.formats.plain import get_plain_dict
+from gendiff.formats.stylish import stylize
 
 
 def format_diff(difference, output_format='stylish'):
     if output_format == 'stylish':
-        formatted_diff = stylish.stylize(difference)
+        formatted_diff = stylize(difference)
         return formatted_diff
     elif output_format == 'plain':
-        formatted_diff = plain.get_plain_dict(difference)
+        formatted_diff = get_plain_dict(difference)
         return formatted_diff
     elif output_format == 'json':
-        formatted_diff = json.get_json_dict(difference)
+        formatted_diff = get_json_dict(difference)
         return formatted_diff
