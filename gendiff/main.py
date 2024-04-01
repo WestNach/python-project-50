a3e3924799e5bcb920fa9e9be39995dcd8f0b0dd
root <root@DESKTOP-LDK1LH6>
2024-04-01 23:39:08 +0500

    rework open_file into open_and_parse_data

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,27 +1,15 @@
-import yaml
-import json
-import os
+
 from gendiff.formats.formatting import format_diff
 from gendiff.diff import get_diff
+import os
+from gendiff.parser import open_and_parse_data
 
 
-# Open and parse file based on extension
-def open_file(path):
-    _, extension = (os.path.splitext(path))
-    if extension == ".json":
-        with open(path) as f:
-            file = json.load(f)
-    elif extension == ".yaml" or extension == ".yml":
-        with open(path) as f:
-            file = yaml.load(f, Loader=yaml.FullLoader)
-    else:
-        raise ValueError(f'Unsupported file format: {extension}')
-    return file
-
-
-def generate_diff(first_file, second_file, output_format="stylish"):
-    file1 = open_file(first_file)
-    file2 = open_file(second_file)
+def generate_diff(path1, path2, output_format="stylish"):
+    _, extension1 = (os.path.splitext(path1))
+    _, extension2 = (os.path.splitext(path2))
+    file1 = open_and_parse_data(path1, extension1)
+    file2 = open_and_parse_data(path2, extension2)
     difference = get_diff(file1, file2)
     result_differance = format_diff(difference, output_format)
     return result_differance
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,4 +1,18 @@
 import argparse
+import yaml
+import json
+
+
+def open_and_parse_data(path, extension):
+    if extension == ".json":
+        with open(path) as f:
+            file = json.load(f)
+    elif extension == ".yaml" or extension == ".yml":
+        with open(path) as f:
+            file = yaml.load(f, Loader=yaml.FullLoader)
+    else:
+        raise ValueError(f'Unsupported file format: {extension}')
+    return file
 
 
 # Function to parse command line arguments
@@ -9,8 +23,8 @@ def parse():
         description='Generate difference between two files')
 
     # Add arguments to the parser
-    parser.add_argument('path1', help='path to the first file')
-    parser.add_argument('path2', help='path to the second file')
+    parser.add_argument('path1', help='path to the data1')
+    parser.add_argument('path2', help='path to the data2')
 
     # Add optional argument with format choices
     parser.add_argument('-f', '--format', choices=['plain', 'stylish', 'json'],
